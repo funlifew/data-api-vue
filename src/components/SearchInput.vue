e336309a76e8111b79082b54e93e469498721057
Mehdi Radfar <metiii.py@gmail.com>
2025-12-23 16:54:48 +0330

    📦Adding SearchInput component

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { watch } from 'vue';
+
+    const props = defineProps({
+        modelValue: String,
+    });
+
+    const emits = defineEmits(['update:modelValue']);
+
+    watch(() => props.modelValue, val => emits('update:modelValue', val));
+</script>
+
+<template>
+    <div class="
+        relative
+        w-full
+        sm:w-64
+    ">
+        <i class="
+            bx bxs-search
+            absolute
+            left-3 top-1/2
+            -translate-y-1/2
+            text-gray-500
+        "></i>
+
+        <input
+            v-model="modelValue"
+            type="text"
+            placeholder="Search users..."
+            class="
+                pl-10 pr-4 py-2
+                border
+                rounded-lg
+                focus:outline-none
+                focus:ring
+                w-full
+            "
+            
+        />
+    </div>
+</template>
